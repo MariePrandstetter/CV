cce1cd1ff703c5e039be1f722a77ddd57864860d
Marie Begue <marie.prandstetter@gmail.com>
2020-06-10 13:11:33 +0200

    skill bars to be continued

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,12 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+$(function() {
+  $('progress').each(function() {
+    var max = $(this).val();
+    $(this).val(0).animate({ value: max }, { duration: 2000, easing: 'easeOutCirc' });
+      });
+});
+
+// trouver où intégrer JS
