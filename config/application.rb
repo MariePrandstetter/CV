f263b7ebeba9f49248afb5e1a016c65988016e15
Marie Begue <marie.prandstetter@gmail.com>
2020-10-13 14:41:32 +0200

    pus

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require 'rails/all'
 
 # pb heroku
 
-config.assets.initialize_on_precompile = false
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,5 +18,6 @@ module CV
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+      config.assets.initialize_on_precompile = false
   end
 end
